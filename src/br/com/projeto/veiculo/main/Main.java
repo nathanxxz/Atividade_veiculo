25883f4ce58dbc1a6d22015f67f9a48ac652fe64
Fabricio Nathan <fabricionathan749@gmail.com>
2025-03-23 14:18:27 -0300

    projeto pronto

--- src/br/com/projeto/veiculo/main/Main.java
+++ src/br/com/projeto/veiculo/main/Main.java
@@ -1,4 +1,30 @@
 package br.com.projeto.veiculo.main;
 
+import br.com.projeto.veiculo.model.Veiculo;
+
 public class Main {
+    public static  void main(String[]args){
+        Veiculo veiculo1=new Veiculo("Moto","Vermelha",15000,2);
+        Veiculo veiculo2=new Veiculo("Carro","Preto",40000,5);
+        Veiculo veiculo3=new Veiculo("Onibus","Amarelo",1000000,70);
+        Veiculo veiculo4=new Veiculo("Aviao","Azul",1000000000,200);
+        Veiculo veiculo5=new Veiculo("Bicicleta","Branca",7500,1);
+
+
+        System.out.println("EXIBINDO INFORMACOES DOS VEICULOS");
+        System.out.println();
+        veiculo1.corValiosa("Vermelho");
+        System.out.println();
+        System.out.println();
+        veiculo1.exibirInfor();
+        System.out.println();
+        veiculo2.exibirInfor();
+        System.out.println();
+        veiculo3.exibirInfor();
+        System.out.println();
+        veiculo4.exibirInfor();
+        System.out.println();
+        veiculo5.exibirInfor();
+
+    }
 }
--- src/br/com/projeto/veiculo/model/Veiculo.java
+++ src/br/com/projeto/veiculo/model/Veiculo.java
@@ -1,4 +1,29 @@
 package br.com.projeto.veiculo.model;
 
 public class Veiculo {
+    public String tipo;
+    public String cor;
+    public double preco;
+    public int qtddepassageiros;
+
+    public Veiculo(String tipo,String cor,double preco,int qtddepassageiros){
+        this.tipo=tipo;
+        this.cor=cor;
+        this.preco=preco;
+        this.qtddepassageiros=qtddepassageiros;
+
+    }
+    public void exibirInfor(){
+        System.out.printf("É um %s, a cor é %s, o valor é %.2f, e  a quantidade de passageiros é %d",this.tipo,this.cor,this.preco,this.qtddepassageiros);
+    }
+    public void corValiosa(String cor){
+        if (this.cor==cor){
+            this.preco*=0.20;
+            System.out.printf("A cor valiosa é %s, e o preco vai subir 20 porcento %.2f",this.cor,this.preco);
+        }
+        else {
+            System.out.println("A cor escolhida nao é rara, vai continuar no preco padrao");
+        }
+
+    }
 }
